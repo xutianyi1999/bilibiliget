a62bb9769ee1c5f6c2589474f97196f5ff658ba0
xutianyi1999 <853122726@qq.com>
2019-12-09 13:29:48 +0800

    update

--- src/main/java/BilibiliGetApplication.java
+++ src/main/java/BilibiliGetApplication.java
@@ -1,135 +1,164 @@
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import common.Common;
-import crawler.VideoCrawler;
-import download.DownloadUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.util.function.Tuple2;
-import thread.MThread;
+import reactor.core.publisher.MonoSink;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.RandomAccessFile;
 import java.net.URL;
-import java.util.*;
+import java.net.URLConnection;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class BilibiliGetApplication {
 
-  private static Logger logger = LogManager.getLogger(BilibiliGetApplication.class);
+  private static final Logger LOGGER = LogManager.getLogger(BilibiliGetApplication.class);
+  private static String target;
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     if (args.length == 0) {
-      logger.error("Please enter URL");
+      LOGGER.error("Please enter URL");
       return;
     }
 
-    Mono.<String>create(sink -> {
-      try {
-        logger.info("Crawler start");
+    LOGGER.info("Process start");
+    target = args[0];
 
-        Optional<String> optional = Jsoup.connect(args[0])
-          .header("User-Agent", Common.USER_AGENT)
-          .get()
-          .getElementsByTag("script")
-          .stream()
-          .map(Element::data)
-          .filter(data -> data.contains("window.__INITIAL_STATE__") || data.contains("window.__playinfo__"))
-          .findFirst();
+    Flux.fromStream(
+      Jsoup.connect(target)
+        .header("User-Agent", Commons.USER_AGENT)
+        .get()
+        .getElementsByTag("script")
+        .stream()
+    )
+      .map(Element::data)
+      .filter(data -> data.contains("window.__INITIAL_STATE__") || data.contains("window.__playinfo__"))
+      .next()
+      .flatMap(data -> Mono.<JSONObject>create(sink -> {
+          LOGGER.info("Get json data");
 
-        if (optional.isPresent()) {
-          sink.success(optional.get());
-        } else {
-          sink.error(new Throwable("Get json fault"));
-        }
-      } catch (IOException e) {
-        sink.error(e);
-      }
-    }).flatMap(data -> Mono.<JSONObject>create(sink -> {
-        String[] str = data.split("=", 2);
+          String[] str = data.split("=", 2);
 
-        if (data.contains("window.__playinfo__")) {
-          sink.success(JSON.parseObject(str[1]));
-        } else {
-          JSONObject tempData = JSON.parseObject(
-            str[1].split(";", 2)[0]
-          );
+          if (data.contains("window.__playinfo__")) {
+            sink.success(JSON.parseObject(str[1]));
+          } else {
+            JSONObject tempData = JSON.parseObject(
+              str[1].split(";", 2)[0]
+            );
 
-          JSONObject videoData = tempData.getJSONObject("videoData");
-
-          try (InputStream inputStream = Common.getUrlConnection(
-            new URL(Common.PLAYER_API + "?&avid=" + tempData.get("aid") + "&cid=" + videoData.get("cid") + "&fnval=16")
-          ).getInputStream()) {
-            sink.success(JSON.parseObject(inputStream, Map.class));
-          } catch (Exception e) {
-            sink.error(e);
+            JSONObject videoData = tempData.getJSONObject("videoData");
+            sink.success(getJsonObject(tempData, videoData));
           }
-        }
-      })
-    ).flatMap(json -> Mono.<Tuple2<URL, URL>>create(sink -> {
+        })
+      ).flatMap(json -> {
+      LOGGER.info("Start download");
+
       JSONObject data = json.getJSONObject("data");
       JSONObject dash = data.getJSONObject("dash");
 
       if (dash != null) {
-        dash.getJSONArray("video").toJavaList(JSONObject.class)
-          .stream()
-          .max(Comparator.comparing(a -> a.getLong("bandwidth")))
-          .get();
+        Function<String, Consumer<MonoSink<File>>> f =
+          key -> sink -> sink.success(download(getMax(dash.getJSONArray(key))));
+
+        return Mono.zip(
+          Mono.create(f.apply("video")),
+          Mono.create(f.apply("audio"))
+        ).map(tuple -> List.of(tuple.getT1(), tuple.getT2()));
       } else {
-
+        return Mono.create(sink ->
+          sink.success(List.of(download(data.getJSONArray("durl").getJSONObject(0).getString("url"))))
+        );
       }
-    }));
-    Map<String, URL> map = VideoCrawler.getPlayerUrl();
+    }).subscribe(list -> {
+      if (list.size() == 2) {
+        LOGGER.info("Call ffmpeg");
 
-    if (map == null) {
-      logger.error("Get url fault");
-      return;
-    }
+        File videoFile = list.get(0);
+        File audioFile = list.get(1);
+        String finalFile = "final_" + videoFile.getName();
+        String command = "ffmpeg -i " + videoFile.getAbsolutePath() + " -i " + audioFile.getAbsolutePath() + " -c copy ./" + finalFile;
 
-    logger.info("Start download");
+        try {
+          Runtime.getRuntime().exec(command).waitFor();
 
-    if (map.get(Common.AUDIO) != null) {
-      new MThread(map.get(Common.VIDEO), Common.VIDEO).start();
-      new MThread(map.get(Common.AUDIO), Common.AUDIO).start();
-
-      try {
-        while (Thread.activeCount() > 1) {
-          Thread.sleep(500);
+          if (!videoFile.delete() || !audioFile.delete()) {
+            LOGGER.error("Delete temp error");
+          }
+          LOGGER.info("Success " + finalFile);
+        } catch (Exception e) {
+          LOGGER.error("Call ffmpeg error");
+          e.printStackTrace();
         }
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-        logger.error("Thread error");
-        return;
+      } else {
+        LOGGER.info("Success " + list.get(0).getName());
       }
-
-      logger.info("Call ffmpeg");
-      String command = "ffmpeg -i " + Common.VIDEO_FILE.getAbsolutePath() + " -i " + Common.AUDIO_FILE.getAbsolutePath() + " -c copy ./final_" + Common.VIDEO_FILE.getName();
-      Runtime runtime = Runtime.getRuntime();
-
-      try {
-        runtime.exec(command).waitFor();
-      } catch (Exception e) {
-        logger.error("Call ffmpeg error");
-        e.printStackTrace();
-        return;
-      }
-
-      if (!Common.VIDEO_FILE.delete() || !Common.AUDIO_FILE.delete()) {
-        logger.error("File deletion failed");
-      }
-    } else {
-      DownloadUtils.downloadFile(map.get(Common.VIDEO), Common.VIDEO);
-    }
-    logger.info("Finish");
+    }, throwable -> {
+      LOGGER.error("Run error");
+      throwable.printStackTrace();
+    });
   }
 
-  private static Map getMax(List list) {
-    return (Map) Collections.max(list, (o1, o2) -> {
-      Map object1 = (Map) o1;
-      Map object2 = (Map) o2;
-      return ((Integer) object1.get("bandwidth")).compareTo((Integer) object2.get("bandwidth"));
-    });
+  private static JSONObject getJsonObject(JSONObject tempData, JSONObject videoData) {
+    try (InputStream inputStream = getUrlConnection(
+      new URL(Commons.PLAYER_API + "?&avid=" + tempData.get("aid") + "&cid=" + videoData.get("cid") + "&fnval=16")
+    ).getInputStream()) {
+      return JSON.parseObject(inputStream, JSONObject.class);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static String getMax(JSONArray jsonArray) {
+    return jsonArray.toJavaList(JSONObject.class).stream()
+      .max(Comparator.comparing(a -> a.getLong("bandwidth")))
+      .map(json -> json.getString("baseUrl"))
+      .orElse(null);
+  }
+
+  private static File download(String url) {
+    try {
+      URL urlObject = new URL(url);
+      URLConnection urlConnection = getUrlConnection(urlObject);
+      File file = new File("./", new File(urlObject.getPath()).getName().replace("m4s", "mp4"));
+
+      try (
+        RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
+        FileChannel fileChannel = randomAccessFile.getChannel();
+        InputStream inputStream = urlConnection.getInputStream();
+        ReadableByteChannel urlChannel = Channels.newChannel(inputStream)
+      ) {
+        fileChannel.transferFrom(urlChannel, 0, urlConnection.getContentLengthLong());
+        return file;
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static URLConnection getUrlConnection(URL url) {
+    try {
+      URLConnection urlConnection = url.openConnection();
+      urlConnection.setConnectTimeout(Commons.TIMEOUT);
+      urlConnection.setReadTimeout(Commons.TIMEOUT);
+      urlConnection.addRequestProperty("Referer", target);
+      urlConnection.addRequestProperty("Origin", Commons.ORIGIN);
+      urlConnection.addRequestProperty("User-Agent", Commons.USER_AGENT);
+      return urlConnection;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 }
---
+++ src/main/java/Commons.java
@@ -0,0 +1,7 @@
+public interface Commons {
+
+  String ORIGIN = "https://www.bilibili.com";
+  String PLAYER_API = "https://api.bilibili.com/x/player/playurl";
+  String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36";
+  int TIMEOUT = 5000;
+}
